6a456e786e41de2871288c1275015fc836b72127
Manoj Nayak <manojvnayak@outlook.in>
2023-11-17 16:00:33 +0530

    update

--- Beta/Exam.py
+++ Beta/Exam.py
@@ -1,38 +1,42 @@
-from flask import Flask, render_template, redirect, request
-
+from flask import Flask, render_template, redirect, request, url_for,session
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+#secret key is
+
+app.config['SECRET_KEY'] ="Examination"
+#adding Database
+#app.config['SQLALCHEMY_DATABASE_URI']
 @app.route("/")
 def home():
     return render_template('index.html')
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    pnr = 0  # Initialize pnr to None
+    pnr = 0
     if request.method == 'POST':
-        # Process the form data
         name = request.form.get('name')
         pnr = request.form.get('pnr')
-        return redirect(f'/Dashboard/{pnr}')
-    return render_template('login.html', pnr=pnr)  # Pass pnr to the template
-
+        session["pnr"] = pnr
+        return redirect("/Dashboard")
+    else:
+        return render_template('login.html')
 @app.route("/register", methods=["POST", "GET"])
 def register():
-    pnr = None  # Initialize pnr to None
     if request.method == 'POST':
-        # Process the form data
-        username = request.form.get('username')
+        name = request.form.get('name')
+        fname ,Mname, Lname = name.split(" ")
         pnr = request.form.get('pnr')
-        return redirect(f'/Dashboard/{pnr}')
-    return render_template('reg.html', pnr=pnr)  # Pass pnr to the template
-
-@app.route("/Dashboard/<int:pnr>", methods=["POST", "GET"])
-def dashboard(pnr):
-    return render_template('Dashboard.html', pnr=pnr)
-
+        session["pnr"] = pnr
+        return redirect('Dashboard')
+    return render_template('reg.html', pnr=pnr)
+@app.route("/Dashboard", methods=["POST", "GET"])
+def dashboard():
+    if "pnr" in session:
+        return render_template('Dashboard.html')
+    else:
+        return redirect(url_for("login"))
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
-
 if __name__ == '__main__':
     app.run(debug=True)
