14a36db33169faec2959a2e1e4ceeaac4182ee87
Manoj Nayak <manojvnayak@outlook.in>
2023-11-23 22:46:02 +0530

    Update Login.sql

--- Beta/Database commands/Login.sql
+++ Beta/Database commands/Login.sql
@@ -2,5 +2,7 @@ use examination;
 show tables;
 desc login_student;
 desc login_teacher;
-insert into login_teacher values ("1032221958@mitwpu.edu.in","Manoj",1032221958);
+insert into login_teacher values ("1032221958@mitwpu.edu.in","Manoj","Man@123");
+-- truncate login_teacher;
 select * from login_teacher;
+select * from login_student;
