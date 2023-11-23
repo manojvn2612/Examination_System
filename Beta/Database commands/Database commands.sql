cce9288f19ca85638c3a33ca6584a137778ad13a
Manoj Nayak <manojvnayak@outlook.in>
2023-11-23 14:41:05 +0530

    Update Database commands.sql

--- Beta/Database commands/Database commands.sql
+++ Beta/Database commands/Database commands.sql
@@ -1,6 +1,12 @@
 use examination;
--- call q_p_create(1);
--- call q_p_create(2);
-use questions;
-show tables;
-desc q1;
+CREATE PROCEDURE q_p_create(IN qid INT)
+BEGIN
+  DECLARE table_name VARCHAR(32);
+  SET table_name = CONCAT('Q', qid);
+
+  SET @sql = CONCAT('CREATE TABLE questions.', table_name, ' (id INT PRIMARY KEY AUTO_INCREMENT, question_text TEXT)');
+
+  PREPARE stmt FROM @sql;
+  EXECUTE stmt;
+  DEALLOCATE PREPARE stmt;
+END$$
