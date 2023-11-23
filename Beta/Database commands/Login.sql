e9d1a4b339cd536f8edee91a25a7d4f4448f3869
Manoj Nayak <manojvnayak@outlook.in>
2023-11-23 22:13:01 +0530

    Create Login.sql

---
+++ Beta/Database commands/Login.sql
@@ -0,0 +1,6 @@
+use examination;
+show tables;
+desc login_student;
+desc login_teacher;
+insert into login_teacher values ("1032221958@mitwpu.edu.in","Manoj",1032221958);
+select * from login_teacher;
