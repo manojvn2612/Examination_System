f88c9cea1b7d952d1654bfd4f9da87e80f858116
Manoj Nayak <manojvnayak@outlook.in>
2023-11-29 00:15:13 +0530

    Update Procedures.sql

--- Beta/Database commands/Procedures.sql
+++ Beta/Database commands/Procedures.sql
@@ -1,14 +1,15 @@
-use examination;
--- create a procedure to create a question paper
-delimiter $$
+USE examination;
+DELIMITER $$
+
 CREATE PROCEDURE q_p_create(IN qid INT)
 BEGIN
   DECLARE table_name VARCHAR(32);
   SET table_name = CONCAT('Q', qid);
-  SET @sql = CONCAT('CREATE TABLE IF NOT EXISTS questions.', table_name, ' (id INT PRIMARY KEY AUTO_INCREMENT = 1, question_text TEXT)');
+  SET @sql = CONCAT('CREATE TABLE IF NOT EXISTS questions.', table_name, ' (qid INT PRIMARY KEY AUTO_INCREMENT, question_text TEXT, option_a TINYINT(1), option_b TINYINT(1), option_c TINYINT(1), option_d TINYINT(1), correct CHAR(2))');
 
   PREPARE stmt FROM @sql;
   EXECUTE stmt;
   DEALLOCATE PREPARE stmt;
 END$$
-delimiter ;
+
+DELIMITER ;
