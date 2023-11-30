58306037cd8bae9879bdea01f0d01f29a792cbf9
Manoj Nayak <manojvnayak@outlook.in>
2023-11-30 23:44:20 +0530

    Update Procedures.sql

--- Beta/Database commands/Procedures.sql
+++ Beta/Database commands/Procedures.sql
@@ -1,11 +1,12 @@
 USE examination;
+-- drop procedure q_p_create;
 DELIMITER $$
 
 CREATE PROCEDURE q_p_create(IN qid INT)
 BEGIN
   DECLARE table_name VARCHAR(32);
   SET table_name = CONCAT('Q', qid);
-  SET @sql = CONCAT('CREATE TABLE IF NOT EXISTS questions.', table_name, ' (qid INT PRIMARY KEY AUTO_INCREMENT, question_text TEXT, option_a TEXT, option_b TEXT(), option_c TEXT(), option_d TEXT(), correct CHAR(2))');
+  SET @sql = CONCAT('CREATE TABLE IF NOT EXISTS questions.', table_name, ' (qid INT PRIMARY KEY AUTO_INCREMENT, question_text TEXT, option_a TEXT, option_b TEXT, option_c TEXT, option_d TEXT, correct CHAR(2))');
 
   PREPARE stmt FROM @sql;
   EXECUTE stmt;
