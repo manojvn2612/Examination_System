85d83d823b3cf0e74bfcb6b9dc4caf451bc86161
Manoj Nayak <manojvnayak@outlook.in>
2023-11-30 12:31:24 +0530

    Update Procedures.sql

--- Beta/Database commands/Procedures.sql
+++ Beta/Database commands/Procedures.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE q_p_create(IN qid INT)
 BEGIN
   DECLARE table_name VARCHAR(32);
   SET table_name = CONCAT('Q', qid);
-  SET @sql = CONCAT('CREATE TABLE IF NOT EXISTS questions.', table_name, ' (qid INT PRIMARY KEY AUTO_INCREMENT, question_text TEXT, option_a TINYINT(1), option_b TINYINT(1), option_c TINYINT(1), option_d TINYINT(1), correct CHAR(2))');
+  SET @sql = CONCAT('CREATE TABLE IF NOT EXISTS questions.', table_name, ' (qid INT PRIMARY KEY AUTO_INCREMENT, question_text TEXT, option_a TEXT(1), option_b TEXT(), option_c TEXT(), option_d TEXT(), correct CHAR(2))');
 
   PREPARE stmt FROM @sql;
   EXECUTE stmt;
